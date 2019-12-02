1be8eab3a7f67861fa3d2a8a3ae75cefc72714bf
jaegwang <jaegwang@outlook.com>
2019-12-03 07:13:18 +0900

    .

--- GetBoraRpmVersion.py
+++
@@ -1,9 +0,0 @@
-f = open("Bora.spec", "r")
-
-line = f.readline()
-
-line = f.readline()
-version = line.split(": ")[1]
-print(version)
-
-f.close()
--- dev_publish.sh
+++
@@ -1,6 +0,0 @@
-
-rm -rf /netapp/backstage/dev/apps/houdini2/tools/Bora/lib;
-rm -rf /netapp/backstage/dev/apps/houdini2/tools/Bora/houdini;
-
-cp -rf build/lib /netapp/backstage/dev/apps/houdini2/tools/Bora/lib;
-cp -rf build/houdini /netapp/backstage/dev/apps/houdini2/tools/Bora/houdini;
